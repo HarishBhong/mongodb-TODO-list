ca3049e813cd498e476bd1300330052722ec8865
HarishBhong <harishbhong@gmail.com>
2024-04-14 07:22:53 +0530

    adding functions

--- app.js
+++ app.js
@@ -22,4 +22,49 @@ const todo = new item({
   name: "Create some videos"
 });
 
-todo.save();
+const todo2 = new item({
+  name: "Learn DSA"
+});
+
+const todo3 = new item({
+  name: "GO to GYM"
+});
+
+const todo4 = new item({
+  name: "Netflix and CHILL"
+});
+
+// todo2.save();
+// todo3.save();
+// todo4.save();
+
+app.get("/",function(req,res){
+    item.find({},function(err,foundItems){
+        if(err){
+          console.log(err);
+        }
+        else{
+          res.render("list",{dayej : foundItems});
+        }
+    });
+});
+
+//creating post request
+app.post("/",function(req,res){
+  const itemName = req.body.ele1;
+  const todo4 = new item ({
+      name : itemName
+  });
+  todo4.save();
+  res.redirect("/");
+});
+//we have created item named variable and what ever value is entered in input box by user we are storing it in 'item' variable. input box is accessed by command : req,body.ele1 .ele1 is in the list.ejs file of view folder
+
+
+// to display record on user screen when user will make a request. this is done by creating server using listen() function
+app.listen("3000",function(){
+  console.log("server is running")
+});
+
+//server will run at port no:3000
+//now request is created using get function, which is written above, but its written after writting listen function
